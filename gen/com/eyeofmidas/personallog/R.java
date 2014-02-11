b0cc42d3d684d97757c2fd68a4f9061a7a7f8e06
Justin Gilman <eyeofmidas@gmail.com>
2014-02-11 11:35:38 -0500

    replacing libogg-vorbis with org.xiph.vorbis.encoder, using intents to open selected log files

--- gen/com/eyeofmidas/personallog/R.java
+++ gen/com/eyeofmidas/personallog/R.java
@@ -8,6 +8,12 @@
 package com.eyeofmidas.personallog;
 
 public final class R {
+    public static final class array {
+        public static final int available_bitrates=0x7f040003;
+        public static final int available_channel_configurations=0x7f040001;
+        public static final int available_qualities=0x7f040002;
+        public static final int available_sample_rates=0x7f040000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -17,22 +23,43 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int icon=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int logList=0x7f080001;
-        public static final int recordNewLogButton=0x7f080000;
+        public static final int action_settings=0x7f090015;
+        public static final int available_bitrates_layout=0x7f09000d;
+        public static final int available_qualities_layout=0x7f09000a;
+        public static final int bitrate_spinner=0x7f09000f;
+        public static final int channel_config_spinner=0x7f090005;
+        public static final int encoding_type_radio_group=0x7f090007;
+        public static final int logList=0x7f090001;
+        public static final int log_area=0x7f090014;
+        public static final int quality_spinner=0x7f09000c;
+        public static final int recordNewLogButton=0x7f090000;
+        public static final int sample_rate_spinner=0x7f090003;
+        public static final int start_playing_button=0x7f090012;
+        public static final int start_recording_button=0x7f090010;
+        public static final int stop_playing_button=0x7f090013;
+        public static final int stop_recording_button=0x7f090011;
+        public static final int textView=0x7f090002;
+        public static final int textView1=0x7f090006;
+        public static final int textView2=0x7f090004;
+        public static final int textView3=0x7f09000b;
+        public static final int textView4=0x7f09000e;
+        public static final int with_bitrate=0x7f090009;
+        public static final int with_quality=0x7f090008;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -63,10 +90,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- src/com/eyeofmidas/personallog/MainActivity.java
+++ src/com/eyeofmidas/personallog/MainActivity.java
@@ -1,51 +1,166 @@
 package com.eyeofmidas.personallog;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.Locale;
 
-import android.os.Bundle;
+import org.xiph.vorbis.recorder.VorbisRecorder;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaRecorder;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
-import android.view.ViewGroup.LayoutParams;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
-import android.widget.TableLayout;
-import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	public static final String TAG = "PersonalLog";
+	private boolean isRecording = false;
+	private String logDirectory;
+	private Button recordingButton;
+	private File appDirectory;
+	private ListView logList;
+	private VorbisRecorder vorbisRecorder;
+	private Handler recordingHandler;
+	private SimpleDateFormat simpleFileDateFormatter;
+	private SimpleDateFormat simpleDisplayDateFormatter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		final Button button = (Button) findViewById(R.id.recordNewLogButton);
-		button.setOnClickListener(new View.OnClickListener() {
-			private boolean isRecording = false;
+		logDirectory = Environment.getExternalStorageDirectory().toString() + "/" + "PersonalLog";
+
+		recordingButton = (Button) findViewById(R.id.recordNewLogButton);
+		recordingButton.setOnClickListener(new View.OnClickListener() {
 
 			public void onClick(View v) {
-				if (!this.isRecording) {
-					this.isRecording = true;
-					button.setText(R.string.stop_recording);
+				if (!isRecording) {
+					isRecording = true;
+					startRecording();
+
 				} else {
-					this.isRecording = false;
-					button.setText(R.string.record_new_log);
+					isRecording = false;
+					stopRecording();
 				}
 			}
 		});
 
-		ListView logList = (ListView) findViewById(R.id.logList);
+		appDirectory = new File(logDirectory);
+		// have the object build the directory structure, if needed.
+		if (!appDirectory.mkdirs() && !appDirectory.isDirectory()) {
+			Log.e(TAG, "The directory " + logDirectory + " does not exist and I could not create it.");
+		}
+
+		simpleFileDateFormatter = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault());
+		simpleDisplayDateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault());
+
+		logList = (ListView) findViewById(R.id.logList);
+		logList.setAdapter(getUpdatedAdapter());
+		logList.setOnItemClickListener(getClickListener());
+
+		recordingHandler = new Handler() {
+			@Override
+			public void handleMessage(Message msg) {
+				switch (msg.what) {
+				case VorbisRecorder.START_ENCODING:
+					Log.d(TAG, "Starting to encode");
+					break;
+				case VorbisRecorder.STOP_ENCODING:
+					Log.d(TAG, "Stopping the encoder");
+					break;
+				case VorbisRecorder.UNSUPPORTED_AUDIO_TRACK_RECORD_PARAMETERS:
+					Log.d(TAG, "You're device does not support this configuration");
+					break;
+				case VorbisRecorder.ERROR_INITIALIZING:
+					Log.d(TAG, "There was an error initializing.  Try changing the recording configuration");
+					break;
+				case VorbisRecorder.FAILED_FOR_UNKNOWN_REASON:
+					Log.d(TAG, "The encoder failed for an unknown reason!");
+					break;
+				case VorbisRecorder.FINISHED_SUCCESSFULLY:
+					Log.d(TAG, "The encoder has finished successfully");
+					break;
+				}
+			}
+		};
+
+	}
+
+	private ArrayAdapter<String> getUpdatedAdapter() {
+		File[] logFiles = appDirectory.listFiles();
+		Arrays.sort(logFiles, new Comparator<File>() {
+			@Override
+			public int compare(File object1, File object2) {
+				return Long.valueOf(object1.lastModified()).compareTo(object2.lastModified());
+			}
+		});
+
 		ArrayList<String> logData = new ArrayList<String>();
-		logData.add("log 1");
-		logData.add("log 2");
-		logData.add("log 3");
+
+		if (logFiles.length > 0) {
+			for (int i = 0; i < logFiles.length; i++) {
+				// TODO: instead of using filenames, output database info
+
+				//logData.add(simpleDisplayDateFormatter.format(logFiles[i].lastModified()));
+				logData.add(logFiles[i].getName());
+			}
+		} else {
+			logData.add("No logs!");
+		}
 
 		ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, logData);
+		return arrayAdapter;
+	}
 
-		logList.setAdapter(arrayAdapter);
+	protected void stopRecording() {
+		vorbisRecorder.stop();
+		vorbisRecorder = null;
+
+		recordingButton.setText(R.string.record_new_log);
+		logList.setAdapter(getUpdatedAdapter());
+		logList.setOnItemClickListener(getClickListener());
+	}
+
+	private OnItemClickListener getClickListener() {
+		return new AdapterView.OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parentAdapter, View view, int position, long id) {
+				TextView clickedView = (TextView) view;
+				Intent openIntent = new Intent();
+				openIntent.setAction(Intent.ACTION_VIEW);
+				File logFile = new File(logDirectory + "/" + clickedView.getText());
+				openIntent.setDataAndType(Uri.fromFile(logFile), "audio/ogg");
+				startActivity(openIntent);
+			}
+		};
+	}
+
+	protected void startRecording() {
+		String currentDateandTime = simpleFileDateFormatter.format(new Date());
+		recordingButton.setText(R.string.stop_recording);
+
+		String outputFileName = logDirectory + "/log_" + currentDateandTime + ".ogg";
+		vorbisRecorder = new VorbisRecorder(new File(outputFileName), recordingHandler);
+		vorbisRecorder.start(44100, 2, 128000);
 	}
 
 	@Override
